1fc04d34316ad920960195d85522758514223a68
Michelle Boyce <boycem@hep.physics.mcgill.ca>
2017-02-10 15:05:36 -0500

    Added geometric parameters: i.e., reduced magic numbers.

--- waterfallplt.js
+++ waterfallplt.js
@@ -6,17 +6,20 @@ function waterfallPlot(d3_AppendToElement) {
 
    this.wf = new mkRandomWaterFallData();
 
+   // geometric parameters (pixels)
+   this.gMainEdge = 400; // Main plot area is square 
+   this.gMiniEdge = this.gMainEdge/4; // Small-edge length of side plots
+   this.gXBoxEdge = this.gMainEdge/this.wf.xBins; // Width of main plot area boxel
+   this.gYBoxEdge = this.gMainEdge/this.wf.yBins; // Heigth of main area boxel 
+
    // svg container for waterfall plot
-   this.svgContainer = d3_AppendToElement.append("svg").attr("width",575).attr("height",600);
+   this.svgContainer = d3_AppendToElement.append("svg").attr("width",this.gMainEdge+this.gMiniEdge+75).attr("height",this.gMainEdge+this.gMiniEdge+75);
 
    // This is the main waterfall plot svg container, which can be used to scale the whole thing... 
    this.gWaterfallPlotContainer = this.svgContainer.append("g").attr("class","waterfall-plot");
 
    // Main waterfall plot area
-   this.gMainEdge = 400;
-   this.gXBoxEdge = this.gMainEdge/this.wf.xBins;
-   this.gYBoxEdge = this.gMainEdge/this.wf.yBins;
-   this.gMainPlot = this.gWaterfallPlotContainer.append("g").attr("class","wf-main-plot").attr("transform","translate(25,129)");
+   this.gMainPlot = this.gWaterfallPlotContainer.append("g").attr("class","wf-main-plot").attr("transform","translate(25,"+(this.gMiniEdge+29)+")");
    this.xScale = d3.scaleLinear().domain([this.wf.xMin,this.wf.xMax]).range([0,this.gMainEdge]);
    this.yScale = d3.scaleLinear().domain([this.wf.yMin,this.wf.yMax]).range([this.gMainEdge,0]);
    this.xAxis  = d3.axisBottom(this.xScale);
@@ -31,7 +34,7 @@ function waterfallPlot(d3_AppendToElement) {
    this.gMainPlot.append("text").attr("x",this.gMainEdge+7).attr("y",this.gMainEdge+30).text(this.wf.xLabel);
    this.gMainPlot.append("text").attr("x",0).attr("y",0).attr("transform","translate(-7,35) rotate(-90)").text(this.wf.yLabel);
    (function(element,xScale,yScale,wf,gXBoxEdge,gYBoxEdge){
-      element.append("g").attr("class","xy-main-plot").attr("transform","translate(26,-2)")
+      element.append("g").attr("class","xy-main-plot").attr("transform","translate(26,"+(-gYBoxEdge)+")")
          .selectAll("rect").data(wf.data).enter().append("rect").attr("width",gXBoxEdge).attr("height",gYBoxEdge)
          .attr("x",function(d){return xScale(d.x);}).attr("y",function(d){return yScale(d.y);})
          .style("fill",function(d){return d3.rgb(d.jy,d.jy,d.jy).toString();});
@@ -39,14 +42,14 @@ function waterfallPlot(d3_AppendToElement) {
 
    // Top portion of waterfall plot
    this.gTopPlot = this.gWaterfallPlotContainer.append("g").attr("class","wf-top-plot").attr("transform","translate(51,29)");
-   this.uScale = d3.scaleLinear().domain([0,1]).range([100,0]);
+   this.uScale = d3.scaleLinear().domain([0,1]).range([this.gMiniEdge,0]);
    this.uAxis  = d3.axisLeft(this.uScale).tickValues([1]);
    this.gTopPlot.append("g").attr("class","top-left-axis").call(this.uAxis);
    this.gTopPlot.append("line").attr("x1",0).attr("y1",1).attr("x2",this.gMainEdge).attr("y2",1)
       .attr("style","stroke:black;stroke-width:1");
-   this.gTopPlot.append("line").attr("x1",this.gMainEdge).attr("y1",1).attr("x2",this.gMainEdge).attr("y2",100)
+   this.gTopPlot.append("line").attr("x1",this.gMainEdge).attr("y1",1).attr("x2",this.gMainEdge).attr("y2",this.gMiniEdge)
       .attr("style","stroke:black;stroke-width:1");
-   this.gTopPlot.append("text").attr("x",0).attr("y",0).attr("transform","translate(-7,50) rotate(-90)").text(this.wf.iLabel);
+   this.gTopPlot.append("text").attr("x",0).attr("y",0).attr("transform","translate(-7,"+((this.gMiniEdge+5)/2)+") rotate(-90)").text(this.wf.iLabel);
    (function(element,wf,xScale,uScale){
       var topLineFunc = d3.line().x(function(d,i){return xScale(wf.xValue(i));}).y(function(d,i){return uScale(d);})
          .curve(d3.curveLinear);
@@ -57,15 +60,15 @@ function waterfallPlot(d3_AppendToElement) {
 
    // Right portion of waterfall plot
    this.gRightPlot = this.gWaterfallPlotContainer.append("g").attr("class","wf-right-plot")
-      .attr("transform","translate("+(this.gMainEdge+51)+",129)");
-   this.rScale = d3.scaleLinear().domain([0,1]).range([0,100]);
+      .attr("transform","translate("+(this.gMainEdge+51)+","+(this.gMiniEdge+29)+")");
+   this.rScale = d3.scaleLinear().domain([0,1]).range([0,this.gMiniEdge]);
    this.rAxis  = d3.axisTop(this.rScale).tickValues([1]);
    this.gRightPlot.append("g").attr("class","right-top-axis").call(this.rAxis);
-   this.gRightPlot.append("line").attr("x1",101).attr("y1",0).attr("x2",101).attr("y2",this.gMainEdge)
+   this.gRightPlot.append("line").attr("x1",this.gMiniEdge+1).attr("y1",0).attr("x2",this.gMiniEdge+1).attr("y2",this.gMainEdge)
       .attr("style","stroke:black;stroke-width:1");
-   this.gRightPlot.append("line").attr("x1",0).attr("y1",this.gMainEdge).attr("x2",101).attr("y2",this.gMainEdge)
+   this.gRightPlot.append("line").attr("x1",0).attr("y1",this.gMainEdge).attr("x2",this.gMiniEdge+1).attr("y2",this.gMainEdge)
       .attr("style","stroke:black;stroke-width:1");
-   this.gRightPlot.append("text").attr("x",45).attr("y",-5).text(this.wf.iLabel);
+   this.gRightPlot.append("text").attr("x",(this.gMiniEdge-5)/2).attr("y",-5).text(this.wf.iLabel);
    (function(element,wf,rScale,yScale){
       var rightLineFunc = d3.line().x(function(d,i){return rScale(d);}).y(function(d,i){return yScale(wf.yValue(i));})
          .curve(d3.curveLinear);
@@ -75,8 +78,8 @@ function waterfallPlot(d3_AppendToElement) {
    })(this.gRightPlot,this.wf,this.rScale,this.yScale);
 
    // Main plot area xhairs
-   this.gXHairs = this.gWaterfallPlotContainer.append("g").attr("class","main-xhairs").attr("transform","translate(52,129)");
-   (function(element,gMainPlot,xScale,yScale,iScale,iLabel,gMainEdge){
+   this.gXHairs = this.gWaterfallPlotContainer.append("g").attr("class","main-xhairs").attr("transform","translate(52,"+(this.gMiniEdge+29)+")");
+   (function(element,gMainPlot,xScale,yScale,iScale,iLabel,gMainEdge,gMiniEdge){
       element.append("rect").attr("class","main-xhairs-region").attr("x",0).attr("y",0).attr("width",gMainEdge).attr("height",gMainEdge+1)
       .attr("fill","none").style("pointer-events","all")
       .on("mouseover",function(){element.selectAll("line, text").style("display",null);})
@@ -91,7 +94,7 @@ function waterfallPlot(d3_AppendToElement) {
          element.selectAll(".main-y-xhairs").transition().duration(10).attr("y1",yPixels).attr("y2",yPixels);
          element.select(".main-x-xhairs-label").transition().duration(10).attr("x",xPixels).text(x>0?x:-x);
          element.select(".main-y-xhairs-label").transition().duration(10)
-            .attr("transform","translate(505,"+yPixels+") rotate(90)").text(y>0?y:-y);
+            .attr("transform","translate("+(gMainEdge+gMiniEdge+5)+","+yPixels+") rotate(90)").text(y>0?y:-y);
 
          // This is for setting the value of the intensity -- tricky...
          gMainPlot.select("g.xy-main-plot").selectAll("rect")
@@ -123,31 +126,31 @@ function waterfallPlot(d3_AppendToElement) {
       //  .on('→', moveMainXHairs(d3.mouse(this)[0]+2,0))
       //  .on('↓', moveMainXHairs(0,d3.mouse(this)[1]+2))
       //);
-   })(this.gXHairs,this.gMainPlot,this.xScale,this.yScale,this.iScale,this.wf.iLabel,this.gMainEdge); 
+   })(this.gXHairs,this.gMainPlot,this.xScale,this.yScale,this.iScale,this.wf.iLabel,this.gMainEdge,this.gMiniEdge); 
    this.gXHairs.append("line").attr("class","main-x-xhairs")
       .attr("x1",this.gMainEdge/2).attr("y1",0).attr("x2",this.gMainEdge/2).attr("y2",this.gMainEdge)
       .attr("stroke","yellow").attr("stroke-width",1).style("display","none");
    this.gXHairs.append("line").attr("class","main-x-xhairs")
-      .attr("x1",this.gMainEdge/2).attr("y1",-99).attr("x2",this.gMainEdge/2).attr("y2",0)
+      .attr("x1",this.gMainEdge/2).attr("y1",-(this.gMiniEdge-1)).attr("x2",this.gMainEdge/2).attr("y2",0)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
    this.gXHairs.append("line").attr("class","main-y-xhairs")
       .attr("x1",0).attr("y1",this.gMainEdge/2).attr("x2",this.gMainEdge).attr("y2",this.gMainEdge/2)
       .attr("stroke","yellow").attr("stroke-width",1).style("display","none");
    this.gXHairs.append("line").attr("class","main-y-xhairs")
-      .attr("x1",this.gMainEdge).attr("y1",this.gMainEdge/2).attr("x2",this.gMainEdge+100).attr("y2",this.gMainEdge/2)
+      .attr("x1",this.gMainEdge).attr("y1",this.gMainEdge/2).attr("x2",this.gMainEdge+this.gMiniEdge).attr("y2",this.gMainEdge/2)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
-   this.gXHairs.append("text").attr("class","main-x-xhairs-label").attr("x",this.gMainEdge/2).attr("y",-104)
+   this.gXHairs.append("text").attr("class","main-x-xhairs-label").attr("x",this.gMainEdge/2).attr("y",-(this.gMiniEdge+4))
       .style("text-anchor","middle").text(Number(this.xScale.invert(this.gMainEdge/2)).toFixed(1)).style("display","none");
    this.gXHairs.append("text").attr("class","main-y-xhairs-label").attr("x",0).attr("y",0)
       .style("text-anchor","middle").text(Number(this.yScale.invert(this.gMainEdge/2)).toFixed(1))
-      .attr("transform","translate("+(this.gMainEdge+105)+","+(this.gMainEdge/2)+") rotate(90)").style("display","none");
+      .attr("transform","translate("+(this.gMainEdge+this.gMiniEdge+5)+","+(this.gMainEdge/2)+") rotate(90)").style("display","none");
    this.gXHairs.append("text").attr("class","main-z-xhairs").attr("x",this.gMainEdge/2).attr("y",this.gMainEdge/2).text("0")
       .style("fill","yellow").style("display","none");
 
    // Top graph xhairs
    this.gTopXHairs = this.gWaterfallPlotContainer.append("g").attr("class","top-xhairs").attr("transform","translate(52,29)");
-   (function(element,xScale,uScale,gMainEdge){
-      element.append("rect").attr("class","main-xhairs-region").attr("x",0).attr("y",0).attr("width",gMainEdge).attr("height",100)
+   (function(element,xScale,uScale,gMainEdge,gMiniEdge){
+      element.append("rect").attr("class","main-xhairs-region").attr("x",0).attr("y",0).attr("width",gMainEdge).attr("height",gMiniEdge)
          .attr("fill","none").style("pointer-events","all")
          .on("mouseover",function(){element.selectAll("line, text").style("display",null);})
          .on("mouseout", function(){element.selectAll("line, text").style("display","none");})
@@ -162,24 +165,24 @@ function waterfallPlot(d3_AppendToElement) {
             element.select(".top-x-xhairs-label").transition().duration(10).attr("x",xPixels).text(x>0?x:-x);
             element.select(".top-y-xhairs-label").transition().duration(10).attr("y",yPixels).text(y>0?y:-y);
          });
-   })(this.gTopXHairs,this.xScale,this.uScale,this.gMainEdge);
-   this.gTopXHairs.append("line").attr("class","top-x-xhairs").attr("x1",this.gMainEdge/2).attr("y1",0).attr("x2",this.gMainEdge/2).attr("y2",100)
+   })(this.gTopXHairs,this.xScale,this.uScale,this.gMainEdge,this.gMiniEdge);
+   this.gTopXHairs.append("line").attr("class","top-x-xhairs").attr("x1",this.gMainEdge/2).attr("y1",0).attr("x2",this.gMainEdge/2).attr("y2",this.gMiniEdge)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
    this.gTopXHairs.append("line").attr("class","top-x-xhairs")
-      .attr("x1",this.gMainEdge/2).attr("y1",100).attr("x2",this.gMainEdge/2).attr("y2",this.gMainEdge+100)
+      .attr("x1",this.gMainEdge/2).attr("y1",100).attr("x2",this.gMainEdge/2).attr("y2",this.gMainEdge+this.gMiniEdge)
       .attr("stroke","yellow").attr("stroke-width",1).style("display","none");
-   this.gTopXHairs.append("line").attr("class","top-y-xhairs").attr("x1",0).attr("y1",50).attr("x2",this.gMainEdge).attr("y2",50)
+   this.gTopXHairs.append("line").attr("class","top-y-xhairs").attr("x1",0).attr("y1",this.gMiniEdge/2).attr("x2",this.gMainEdge).attr("y2",this.gMiniEdge/2)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
    this.gTopXHairs.append("text").attr("class","top-x-xhairs-label").attr("x",this.gMainEdge/2).attr("y",-4)
       .style("text-anchor","middle").text(Number(this.xScale.invert(this.gMainEdge/2)).toFixed(1)).style("display","none");
-   this.gTopXHairs.append("text").attr("class","top-y-xhairs-label").attr("x",this.gMainEdge+4).attr("y",46)
+   this.gTopXHairs.append("text").attr("class","top-y-xhairs-label").attr("x",this.gMainEdge+4).attr("y",this.gMiniEdge-4)
       .text(Number(this.uScale.invert(50)).toFixed(1)).style("display","none");
 
    // Right graph xhairs
    this.gRightXHairs = this.gWaterfallPlotContainer.append("g")
-      .attr("class","top-xhairs").attr("transform","translate("+(this.gMainEdge+51)+",128)");
-   (function(element,rScale,yScale,gMainEdge){
-      element.append("rect").attr("class","main-xhairs-region").attr("x",0).attr("y",0).attr("width",100).attr("height",gMainEdge)
+      .attr("class","top-xhairs").attr("transform","translate("+(this.gMainEdge+51)+","+(this.gMiniEdge+28)+")");
+   (function(element,rScale,yScale,gMainEdge,gMiniEdge){
+      element.append("rect").attr("class","main-xhairs-region").attr("x",0).attr("y",0).attr("width",gMiniEdge).attr("height",gMainEdge)
       .attr("fill","none").style("pointer-events","all")
       .on("mouseover",function(){element.selectAll("line, text").style("display",null);})
       .on("mouseout", function(){element.selectAll("line, text").style("display","none");})
@@ -193,22 +196,22 @@ function waterfallPlot(d3_AppendToElement) {
          element.selectAll(".right-y-xhairs").transition().duration(10).attr("y1",yPixels).attr("y2",yPixels);
          element.select(".right-x-xhairs-label").transition().duration(10).attr("x",xPixels).text(x>0?x:-x);
          element.select(".right-y-xhairs-label").transition().duration(10)
-           .attr("transform","translate(104,"+yPixels+") rotate(90)").text(y>0?y:-y);
+           .attr("transform","translate("+(gMiniEdge+4)+","+yPixels+") rotate(90)").text(y>0?y:-y);
        });
-   })(this.gRightXHairs,this.rScale,this.yScale,this.gMainEdge);
+   })(this.gRightXHairs,this.rScale,this.yScale,this.gMainEdge,this.gMiniEdge);
    this.gRightXHairs.append("line").attr("class","right-x-xhairs")
-      .attr("x1",50).attr("y1",0).attr("x2",50).attr("y2",this.gMainEdge)
+      .attr("x1",this.gMiniEdge/2).attr("y1",0).attr("x2",this.gMiniEdge/2).attr("y2",this.gMainEdge)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
    this.gRightXHairs.append("line").attr("class","right-y-xhairs")
-      .attr("x1",0).attr("y1",this.gMainEdge/2).attr("x2",100).attr("y2",this.gMainEdge)
+      .attr("x1",0).attr("y1",this.gMainEdge/2).attr("x2",this.gMiniEdge).attr("y2",this.gMainEdge)
       .attr("stroke","black").attr("stroke-width",1).style("display","none");
    this.gRightXHairs.append("line").attr("class","right-y-xhairs")
-      .attr("x1",-this.gMainEdge).attr("y1",this.gMainEdge/2).attr("x2",0).attr("y2",200)
+      .attr("x1",-this.gMainEdge).attr("y1",this.gMainEdge/2).attr("x2",0).attr("y2",this.gMainEdge/2)
       .attr("stroke","yellow").attr("stoke-width",1).style("display","none");
    this.gRightXHairs.append("text").attr("class","right-x-xhairs-label").attr("x",54).attr("y",this.gMainEdge+4)
-      .style("alignment-baseline","hanging").text(Number(this.rScale.invert(50)).toFixed(1)).style("display","none");
+      .style("alignment-baseline","hanging").text(Number(this.rScale.invert(this.gMiniEdge/2)).toFixed(1)).style("display","none");
    this.gRightXHairs.append("text").attr("class","right-y-xhairs-label").attr("x",0).attr("y",0)
-      .attr("text-anchor","middle").attr("transform","translate(104,"+(this.gMainEdge/2)+") rotate(90)")
+      .attr("text-anchor","middle").attr("transform","translate("+(this.gMiniEdge+4)+","+(this.gMainEdge/2)+") rotate(90)")
       .text(Number(this.yScale.invert(200)).toFixed(1)).style("display","none");
 };
 
@@ -253,8 +256,8 @@ function mkRandomWaterFallData () {
    this.iLabel="Jy";
    this.yMin = 0;
    this.yMax = 800;
-   this.xBins = 200; // max 200 bins
-   this.yBins = 200; // max 1024 bins
+   this.xBins = 175; // max 256 bins
+   this.yBins = 175; // max 1024 bins
    this.peakBds = {"min":20,"max":180,"minWidth":5,"maxWidth":20};
    this.peak = Math.random() * (this.peakBds.max - this.peakBds.min) + this.peakBds.min;
    this.peakWidth= Math.random()*(this.peakBds.maxWidth - this.peakBds.minWidth) + this.peakBds.minWidth;
